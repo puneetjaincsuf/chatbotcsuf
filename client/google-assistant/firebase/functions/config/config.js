fb47d54296f9b67d89aa2583d5d6bf73dfb2d0f5
Puneet Jain <punitjain@csu.fullerton.edu>
2018-05-09 14:29:57 -0700

    Dialogflow has upgraded the version to V2 and existing code started breaking. Changed the code to make it complatible with V2

--- client/google-assistant/firebase/functions/config/config.js
+++ client/google-assistant/firebase/functions/config/config.js
@@ -3,11 +3,8 @@ var config = {};
 config.web = {};
 
 config.default_stuff =  ['red','green','blue','apple','yellow','orange','politics'];
-config.serviceURL = '[GOOGLE_CLOUD_PROJECT]';
-config.collegeEndpoint = "colleges'
-config.courseEndpoint = 'specificcourses'
+config.serviceURL = 'https://sample-project-196107.appspot.com/';
+config.collegeEndpoint = 'colleges';
+config.courseEndpoint = 'specificcourses/';
 
 module.exports = config;
-
-
-
